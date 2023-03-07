550e3ddb6cd649f615e47faea2d928afe1aca72c
Nabil-ElMaalemNJIT <nre3@njit.edu>
2023-03-06 19:02:59 -0500

    Milestone1 Files

--- Project/Server.java
+++ Project/Server.java
@@ -1,4 +1,4 @@
-package Module5.Part5;
+package Project;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -10,9 +10,9 @@ import java.util.List;
 public class Server {
     int port = 3001;
     // connected clients
-    // private List<ServerThread> clients = new ArrayList<ServerThread>();
+    //private List<ServerThread> clients = new ArrayList<ServerThread>();
     private List<Room> rooms = new ArrayList<Room>();
-    private Room lobby = null;// default room
+    private Room lobby = null;//default room
 
     private void start(int port) {
         this.port = port;
@@ -20,21 +20,19 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(port);) {
             Socket incoming_client = null;
             System.out.println("Server is listening on port " + port);
-            // Reference server statically
-            Room.server = this;// all rooms will have the same reference
             // create a lobby on start
-            lobby = new Room("Lobby");
-            rooms.add(lobby);
+			lobby = new Room("Lobby", this);
+			rooms.add(lobby);
             do {
                 System.out.println("waiting for next client");
                 if (incoming_client != null) {
                     System.out.println("Client connected");
                     ServerThread sClient = new ServerThread(incoming_client, lobby);
                     sClient.start();
-
-                    joinRoom("lobby", sClient);
+                    
+                    joinRoom("Lobby", sClient);
                     incoming_client = null;
-
+                    
                 }
             } while ((incoming_client = serverSocket.accept()) != null);
         } catch (IOException e) {
@@ -44,75 +42,71 @@ public class Server {
             System.out.println("closing server socket");
         }
     }
-
     /***
-     * Helper function to check if room exists by case insensitive name
-     * 
-     * @param roomName The name of the room to look for
-     * @return matched Room or null if not found
-     */
-    private Room getRoom(String roomName) {
-        for (int i = 0, l = rooms.size(); i < l; i++) {
-            if (rooms.get(i).getName().equalsIgnoreCase(roomName)) {
-                return rooms.get(i);
-            }
-        }
-        return null;
-    }
-
+	 * Helper function to check if room exists by case insensitive name
+	 * 
+	 * @param roomName The name of the room to look for
+	 * @return matched Room or null if not found
+	 */
+	private Room getRoom(String roomName) {
+		for (int i = 0, l = rooms.size(); i < l; i++) {
+			if (rooms.get(i).getRoomName().equalsIgnoreCase(roomName)) {
+				return rooms.get(i);
+			}
+		}
+		return null;
+	}
     /***
-     * Attempts to join a room by name. Will remove client from old room and add
-     * them to the new room.
-     * 
-     * @param roomName The desired room to join
-     * @param client   The client moving rooms
-     * @return true if reassign worked; false if new room doesn't exist
-     */
-    protected synchronized boolean joinRoom(String roomName, ServerThread client) {
-        Room newRoom = roomName.equalsIgnoreCase("lobby")?lobby:getRoom(roomName);
-        Room oldRoom = client.getCurrentRoom();
-        if (newRoom != null) {
-            if (oldRoom != null) {
-                System.out.println(client.getName() + " leaving room " + oldRoom.getName());
-                oldRoom.removeClient(client);
-            }
-            System.out.println(client.getName() + " joining room " + newRoom.getName());
-            newRoom.addClient(client);
-            return true;
-        } else {
-            client.sendMessage("Server",
-                    String.format("Room %s wasn't found, please try another", roomName));
-        }
-        return false;
-    }
-
+	 * Attempts to join a room by name. Will remove client from old room and add
+	 * them to the new room.
+	 * 
+	 * @param roomName The desired room to join
+	 * @param client   The client moving rooms
+	 * @return true if reassign worked; false if new room doesn't exist
+	 */
+	protected synchronized boolean joinRoom(String roomName, ServerThread client) {
+		Room newRoom = roomName.equalsIgnoreCase("lobby")?lobby:getRoom(roomName);
+		Room oldRoom = client.getCurrentRoom();
+		if (newRoom != null) {
+			if (oldRoom != null) {
+				System.out.println(client.getName() + " leaving room " + oldRoom.getRoomName());
+				oldRoom.removeClient(client);
+			}
+			System.out.println(client.getName() + " joining room " + newRoom.getRoomName());
+			newRoom.addClient(client);
+			return true;
+		}
+        /* Not Server's responsibility to inform client (ServerThread)
+        else{
+            client.send(String.format("Room %s wasn't found, please try another", roomName));
+        }*/
+		return false;
+	}
     /***
-     * Attempts to create a room with given name if it doesn't exist already.
-     * 
-     * @param roomName The desired room to create
-     * @return true if it was created and false if it exists
-     */
-    protected synchronized boolean createNewRoom(String roomName) {
-        if (getRoom(roomName) != null) {
-            // TODO can't create room
-            System.out.println(String.format("Room %s already exists", roomName));
-            return false;
-        } else {
-            Room room = new Room(roomName);
-            rooms.add(room);
-            System.out.println("Created new room: " + roomName);
-            return true;
+	 * Attempts to create a room with given name if it doesn't exist already.
+	 * 
+	 * @param roomName The desired room to create
+	 * @return true if it was created and false if it exists
+	 */
+	protected synchronized boolean createNewRoom(String roomName) {
+		if (getRoom(roomName) != null) {
+			// TODO can't create room
+			System.out.println(String.format("Room %s already exists", roomName));
+			return false;
+		} else {
+			Room room = new Room(roomName, this);
+			rooms.add(room);
+			System.out.println("Created new room: " + roomName);
+			return true;
+		}
+	}
+    protected synchronized void removeRoom(Room r){
+        if(rooms.removeIf(room->room==r)){
+            System.out.println("Removed empty room " + r.getRoomName());
         }
     }
-
-    protected synchronized void removeRoom(Room r) {
-        if (rooms.removeIf(room -> room == r)) {
-            System.out.println("Removed empty room " + r.getName());
-        }
-    }
-
     protected synchronized void broadcast(String message) {
-        if (processCommand(message)) {
+        if(processCommand(message)){
 
             return;
         }
@@ -120,18 +114,17 @@ public class Server {
         Iterator<Room> it = rooms.iterator();
         while (it.hasNext()) {
             Room room = it.next();
-            if (room != null) {
+            if(room != null){
                 room.sendMessage(null, message);
             }
         }
     }
 
-    private boolean processCommand(String message) {
+    private boolean processCommand(String message){
         System.out.println("Checking command: " + message);
-        // TODO
+        //TODO
         return false;
     }
-
     public static void main(String[] args) {
         System.out.println("Starting Server");
         Server server = new Server();
